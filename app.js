daf45f04a57ccc41ac10aa59d07b9f0ab618c9d9
pste <pirraste@gmail.com>
2021-03-12 10:35:00 +0100

    [cors]

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
 
+// CORS
+fastify.register(require('fastify-cors'), { 
+  origin: "*"
+})
+
 // Declare a route
 fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
+  const dt = new Date().toISOString()
+  return { message: `Time is: ${dt}` }
 })
 
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(3000)
+    await fastify.listen(3000, "0.0.0.0")
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
